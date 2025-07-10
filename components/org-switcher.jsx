cde7d29e76f25a9028d160e490280fdab795b8fb
sunnyGupta128 <sunnygupta6497@gmail.com>
2025-07-10 21:07:57 +0530

    feat: enhance OrgSwitcher component with organization selection and user authentication

--- components/org-switcher.jsx
+++ components/org-switcher.jsx
@@ -1,17 +1,32 @@
 "use client"
 
-import { useOrganization, useUser } from '@clerk/nextjs'
+import { OrganizationSwitcher, SignedIn, SignIn, useOrganization, useUser } from '@clerk/nextjs'
+import { useParams } from 'next/navigation';
 import React from 'react'
 
 const OrgSwitcher = () => {
     const { isLoaded } = useOrganization();
     const { isLoaded: isUserLoaded } =  useUser();
+    const pathname = useParams()
 
     if (!isLoaded || !isUserLoaded) {
         return null; // or a loading spinner
     }
   return (
-    <div>OrgSwitcher</div>
+    <div>
+        <SignedIn>
+            <OrganizationSwitcher 
+                hidePersonal
+                afterCreateOrganizationUrl="/organization/:slug"
+                afterSelectOrganizationUrl="/organization/:slug"
+                createOrganizationMode={
+                    pathname === "/onboarding" ? "navigation" : "modal"
+                }
+                createOrganizationUrl="/onboarding"
+            />
+
+        </SignedIn>
+    </div>
   )
 }
 
